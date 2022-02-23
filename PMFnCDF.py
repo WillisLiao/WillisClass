444ed01670c4b4d96644b7947a25f0e0f28cf104
Willis Liao <willisrain04@gmail.com>
2022-02-24 01:29:16 +0800

    ok

---
+++ PMFnCDF.py
@@ -0,0 +1,13 @@
+import csv
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+with open('cbb21.csv', newline='') as csvfile:
+
+  # 讀取 CSV 檔內容，將每一列轉成一個 dictionary
+  rows = csv.DictReader(csvfile)
+
+  # 以迴圈輸出指定欄位
+  for row in rows:
+    print(row['G'], row['EFG_D'])
