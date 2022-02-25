fb46446a42756912eb20db1a68f2508a4c6a5ec8
Willis Liao <willisrain04@gmail.com>
2022-02-25 11:23:59 +0800

    ok

--- Person2.py
+++ Person2.py
@@ -1,57 +1,108 @@
 class Person:
-    def __init__(self, name, ID , height, weight, gender, birthday ):
+    def __init__(self, name, ID , height, weight, gender, address, email, phone ):
         self.name = name
-        self.ID = ID
+        self.__ID = ID
         self.height = height
         self.weight = weight
         self.gender = gender
-        self.birthday = birthday
+        self.address = address
+        self.email = email
+        self.__phone = phone
   
     def setName(self, newName):
         self.name = newName
     def getName(self):
-        return print(self.name)
+        return self.name
 
     def setID(self, newID):
-        self.ID = newID
+        self.__ID = newID
     def getID(self):
-        return print(self.ID)
+        return self.__ID
 
     def setHeight(self,newHeight):
         self.height = newHeight
     def getHeight(self):
-        return print(self.height)
+        return self.height
 
     def setWeight(self,newWeight):
         self.weight = newWeight
     def getWeight(self):
-        return print(self.weight)
+        return self.weight
 
     def setGender(self, newGender):
         self.gender = newGender       
     def getGender(self):
-        return print(self.gender)
+        return self.gender
 
-    def setBirthday(self, newBirthday):
-        self.birthday = newBirthday
-    def getBirthday(self):
-        return print(self.birthday)
-        
-        
-    def showInfo(self):
-       print(" name:{}\n ID:{}\n height:{}\n weight:{}\n gender:{}\n birthday:{}".format(self.name, self.ID, self.height, self.weight, self.gender, self.birthday))
-    def getStdBMI(self):
-        print("BMI:",int(self.weight)/((int(self.height)*(0.01))**2))
+    def setAddress(self, newAddress):
+        self.address = newAddress
+    def getAddress(self):
+        return self.address
+
+    def setEmail(self, newEmail):
+        self.email = newEmail
+    def getEmail(self):
+        return self.email    
+
+    def setPhone(self, newPhone):
+        self.__phone = newPhone
+    def getPhone(self):
+        return self.__phone  
+    def StudentInfo(self):
+       print(" name:{}\n ID:{}\n height:{}\n weight:{}\n gender:{}\n address:{}\n email:{}\n phone:{}".format(self.name, self.ID, self.height, self.weight, self.gender, self.birthday))
+person1=Person(input(''), input(''), input(''), input(''), input(''), input(''), input(''), input('') )
+class Student(Person):
+    def __init__(self, name, ID, height, weight, gender, address, email, phone, subject, credit, score ):
+        super().__init__(name, id, height, weight, gender, address, email, phone, subject, credit, score)
+        self.subject = subject
+        self.credit = credit
+        self.score = score
+    def setSubject(self, newSubject):
+        self.subject = newSubject
+    def getSubject(self):
+        return self.subject
+
+    def setCredit(self, newCredit):
+        self.credit = newCredit
+    def getCredit(self):
+        return self.credit
+
+    def setScore(self, newScore):
+        self.score = newScore
+    def getScore(self):
+        return self.score
     
-    id = property(getID, setID)
-    bd = property(getBirthday, setBirthday)
-person1 = Person('Willis', 'F130374671','186', '69', 'male','0414')   #對應上面的參數，self不用
 
-person1.showInfo()
-person1.getStdBMI()
-person1.setGender('Gorilla')
-person1.getGender()
-person1.setBirthday('0918')
-person1.getBirthday()
-person1.id = 'F999999'
-print(person1.id)
+class Teacher(Person):
+    def __init__(self, name, ID, height, weight, gender, address, email, phone, subject, credit, score ):
+        super().__init__(name, id, height, weight, gender, address, email, phone, subject, credit, score)
+        self.subject = subject
+        self.credit = credit
+        self.score = score
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+ #   id = property(getID, setID)
+#bd = property(getBirthday, setBirthday)
